b3fbc2328afddda48b633b671c6ca19412fdfe8c
nic-instruction <nicole.bade@seattlecolleges.edu>
2018-01-11 19:54:00 -0800

    Create apache.sh

---
+++ intro_to_automation/apache.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum -y install httpd mod_ssl                                              # This installs apache with encryption
+systemctl enable httpd                                                    # This enables the apache service
+systemctl start httpd                                                     # This starts the apache server
+
+
+sudo sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                            # Commenting out welcome.conf
+
+sudo echo -e "<html> \n<h1> Welcome, NTI-300 </h1> \n</html>" > index.html      # Creating our index.html
