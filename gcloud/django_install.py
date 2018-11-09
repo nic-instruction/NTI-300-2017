d33551705dff63dca58164eb6e08411f9c18b73d
nic-instruction <nicole.bade@seattlecolleges.edu>
2018-11-08 17:23:50 -0800

    Create django_install.py

---
+++ gcloud/django_install.py
@@ -0,0 +1,30 @@
+import os
+
+def setup_install():
+    print('installing pip and virtualenv so we can give django its own version of python')
+    os.system('rpm -iUvh https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm')
+    os.system('yum -y install python-pip && pip install --upgrade pip')
+    os.system('pip install virtualenv')
+    os.chdir('/opt')
+    os.mkdir('/opt/django')
+    os.chdir('/opt/django')
+    os.system('virtualenv django-env')
+    os.system('chown -R nicolebade /opt/django')   # we're useing shell, because the python builtin chown is kind of lame in this case
+
+def django_install():
+    print('activating virtualenv and installing django after pre-requirements have been met')
+                                                 # You must activate the virtualenv shell every time you perform a command in order for it to work from python.
+    os.system('source /opt/django/django-env/bin/activate && pip install django')
+                                                 # confirm install and start a django project
+    os.chdir('/opt/django')
+    os.system('source /opt/django/django-env/bin/activate ' + \
+              '&& django-admin --version ' + \
+              '&& django-admin startproject project1')
+
+def django_start():
+    print('starting django')
+    os.system('source /opt/django/django-env/bin/activate && python /opt/django/project1/manage.py runserver 0.0.0.0:8000&') 
+
+setup_install()
+django_install()
+django_start()
